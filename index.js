8a5f6582f179f413cba49fdfa5663e94a109992a
muhammadrakib2299 <muhammad.rakib2299@gmail.com>
2026-01-30 08:49:20 +0600

    server setup done

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 const express = require("express");
+const app = express();
+const port = process.env.PORT || 5000;
+
+const cors = require("cors");
+
+// midleware
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Simple CRUD Server runnig");
+});
+
+app.listen(port, () => {
+  console.log(`Server running on ${port}`);
+});
